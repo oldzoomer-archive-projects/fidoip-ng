bb91c2f6fe48b50407bc81bc2f0c0f551d7d5640
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-18 17:18:35 +0300

    Nodelist update support, and nodelist some updates.

--- golded/nodelist.sh
+++ golded/nodelist.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+curl -o INSTALLDIR/fido/nodelist/nodelist.txt "ftp://wfido.ru/nodehist/$(date %Y)/nodelist.$(date %j)"
 echo 'Updating golded nodelist index.'
 INSTALLDIR/usr/bin/gnlnx -C INSTALLDIR/usr/etc/golded+/golded.cfg
 
