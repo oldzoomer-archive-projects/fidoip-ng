917d5244a9b987f361d8a664851401a8e0cf9990
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-19 20:54:32 +0300

    Many refactorings

--- crontask.sh
+++ crontask.sh
@@ -3,36 +3,36 @@
 #   by the GNU general public license. See the file LICENSE for details. */
 
 # Script to automatic install crond tasks from node/crontab.cfg.template-node
-CWD=`pwd`
-HOMEDIR=`cd ../; pwd` #  Fidoip installation directory 
-IDN=`id -un` #  User's name
+CWD=$(pwd)
+HOMEDIR=$(cd ../; pwd) #  Fidoip installation directory 
+IDN=$(id -un) #  User's name
 
 set -e
 set -u
 
 TMPOUT="$(mktemp)" || echo "Error creating temporaty file."
 set +e
-crontab -l  > $TMPOUT
+crontab -l  > "$TMPOUT"
 set -e
 
-if grep "fido\.monthly" $TMPOUT 
+if grep "fido\.monthly" "$TMPOUT" 
 then
     echo
-    echo "I see upper line in crontab for user "$IDN"."
+    echo "I see upper line in crontab for user ""$IDN""."
     echo "Seems fidoip NMS tasks already installed."
     echo "Stop proceeding. If you would like change tasks edit crontab manually."
 else
     echo "Installing fidoip NMS crontab tasks for node..."
 echo
-cat node/crontab.cfg.template-node | grep -v "###" | sed "s|INSTALLDIR|$HOMEDIR|g"  >> $TMPOUT
-crontab $TMPOUT
+cat node/crontab.cfg.template-node | grep -v "###" | sed "s|INSTALLDIR|$HOMEDIR|g"  >> "$TMPOUT"
+crontab "$TMPOUT"
 echo 
 crontab -l
 echo ""
-echo "Done. Crontab tasks for user "$IDN" for fidoip NMS installed successfully!"
+echo "Done. Crontab tasks for user ""$IDN"" for fidoip NMS installed successfully!"
 fi
 
-rm -f $TMPOUT
+rm -f "$TMPOUT"
 
 
 
