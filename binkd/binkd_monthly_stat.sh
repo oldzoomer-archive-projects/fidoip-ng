4eca1e7f8805ced89ca2038d892b7cec1ef91185
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-21 17:05:28 +0300

    Revert "Many refactorings"
    This reverts commit 917d5244a9b987f361d8a664851401a8e0cf9990.

--- binkd/binkd_monthly_stat.sh
+++ binkd/binkd_monthly_stat.sh
@@ -2,7 +2,7 @@
 #   Copyright (c) by Maxim Sokolsky (2:5020/828.777). This file is part of fidoip. It is free software and it is covered
 #   by the GNU general public license. See the file LICENSE for details. */
 
-USERNAME=$(whoami) ; T1="root"
+USERNAME=`whoami` ; T1="root"
 if [ "$T1" = "$USERNAME" ]; then 
 echo  'Please do not run this script as root' ; exit
 fi
